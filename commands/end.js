bfdbed5e128cdb09360ec0f0b5e34232582ff5fa
William Chan <willchan789@gmail.com>
2021-05-10 19:03:05 -0600

    ratio pot system for winnings

--- commands/end.js
+++ commands/end.js
@@ -14,10 +14,28 @@ module.exports = {
         if (isNaN(args[0]))
             return message.channel.send("Invalid choice.");
 
+        var total = 0;
+        var c1total = 0;
+        var c2total = 0;
+        for (const user of user_preds) {
+            var pred = user[1].option;
+            total += user[1].amount;
+            if (pred == 1)
+                c1total += user[1].amount;
+            else if (pred == 2)
+                c2total += user[1].amount;
+        }
+
+        var ratio = 0;
+        if (args[0] == 1)
+            ratio = (total/c1total).toFixed(1);
+        else if (args[0] == 2)
+            ratio = (total/c2total).toFixed(1);
+
         for (const user of user_preds) {
             var pred = user[1].option;
             if (pred == args[0]) {
-                var winning = parseInt(user[1].amount) * 2;
+                var winning = parseInt(user[1].amount) * ratio;
                 try {
                     await profileModel.findOneAndUpdate(
                         {
