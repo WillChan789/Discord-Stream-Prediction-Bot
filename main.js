2bc6f4ca9ba453e3ad54b28a62c6cc89b70d957b
William Chan <willchan789@gmail.com>
2021-05-15 23:05:18 -0600

    save users to json on sigint

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 require('dotenv').config();
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 const mongoose = require('mongoose');
+const profileModel = require('./models/profileSchema');
+const fs = require('fs');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
@@ -21,3 +23,16 @@ mongoose.connect(process.env.MONGODB_SRV, {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+process.on('SIGINT', async function() {
+    console.log('caught int');
+    var db = await profileModel.find({});
+    var dbjson = JSON.stringify(db);
+    try {
+        fs.writeFileSync('users.json', dbjson);
+        console.log('Wrote to users.json');
+    } catch (err) {
+        console.log('Error', err);
+    }
+    process.exit();
+});
